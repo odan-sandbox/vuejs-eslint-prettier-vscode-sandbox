2454754122ba6a4ac730630a03de139a51091996
odan <odan3240@gmail.com>
2020-03-12 21:31:53 +0900

    prettier に寄せる

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,8 @@ module.exports = {
       // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
       // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
       'plugin:vue/recommended',
-      'plugin:prettier/recommended'
+      'plugin:prettier/recommended',
+      'prettier/vue'
     ],
     // required to lint *.vue files
     plugins: ['vue'],
--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <a href="/"><img class="img-logo" src="~/assets/img/xxxxxx.png" alt="XXXXXX"/></a>
+    <a href="/">
+      <img class="img-logo" src="~/assets/img/xxxxxx.png" alt="XXXXXX" />
+    </a>
   </div>
 </template>
